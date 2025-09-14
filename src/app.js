a9428f4e23672bbbfd70c2ebc63f3bf073796577
ganeshkumar222 <ganesh.gmv004@gmail.com>
2025-09-15 05:04:26 +0530

    first api creation

--- src/app.js
+++ src/app.js
@@ -3,15 +3,11 @@ const {connectDb} = require("./config/database")
 const app = express()
 const { adminauth, userauth}= require("./middlewares/auth")
 const User = require("./models/user")
+app.use(express.json())
 app.post("/signup",async(req,res)=>{
- const userObj = {
-    firstName : "Ganesh",
-    lastName : "kumar",
-    email:"ganesh.gmv004@gmail.com",
-    password:"ganesh123#"
- }
+    console.log(req.body)
  //creating a new instance of User model
- const user = new User(userObj)
+ const user = new User(req.body)
  try {
      await user.save()
  res.send("User created sucessfully")
